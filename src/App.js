9d971fb16ff07fac829f9b14796168faa894d66c
abdulmoeed9494 <159937546+abdulmoeed9494@users.noreply.github.com>
2024-02-29 00:01:00 -0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,53 +1,21 @@
-// TodoApp.js
+import React from 'react'
+// import BudgetA2nd from './BudgetA2nd';
+// import BudgetA from './BudgetA';
+// import BudgetApp from './BudgetApp'
+import BudgetName from './BudgetName'
 
-import React, { useState } from 'react';
 
-const TodoApp = () => {
-  // To-Do items ko track karne ke liye state
-  const [todos, setTodos] = useState([]);
-  // Naya To-Do item input field ke liye state
-  const [newTodo, setNewTodo] = useState('');
-
-  // Naya To-Do item add karne wala function
-  const addTodo = () => {
-    if (newTodo.trim() !== '') {
-      setTodos([...todos, newTodo]);
-      setNewTodo('');
-    }
-  };
-
-  // To-Do item ko remove karne wala function
-  const removeTodo = (index) => {
-    const updatedTodos = [...todos];
-    updatedTodos.splice(index, 1);
-    setTodos(updatedTodos);
-  };
-
-  // JSX jo component ka UI represent karta hai
+function App() {
   return (
+      <>
     <div>
-      <h1>React Todo App</h1>
-      {/* To-Do item input field */}
-      <input
-        type="text"
-        value={newTodo}
-        onChange={(e) => setNewTodo(e.target.value)}
-        placeholder="Enter new todo"
-      />
-      {/* Naya To-Do item add karne wala button */}
-      <button onClick={addTodo}>Add Todo</button>
-      {/* To-Do items ko list mein render karna */}
-      <ul>
-        {todos.map((todo, index) => (
-          <li key={index}>
-            {todo}
-            {/* Har To-Do item ke liye remove karne wala button */}
-            <button onClick={() => removeTodo(index)}>Remove</button>
-          </li>
-        ))}
-      </ul>
     </div>
-  );
-};
+    {/* <BudgetApp /> */}
+    {/* <BudgetA /> */}
+    {/* <BudgetA2nd /> */}
+    <BudgetName />
+      </>
+  )
+}
 
-export default TodoApp;
+export default App;
